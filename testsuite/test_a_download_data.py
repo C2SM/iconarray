4baf7d8ae633c7ee4d73a48a8ddbec1aa78131d0
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-05-03 15:30:59 +0200

    remove example plots

--- testsuite/test_a_download_data.py
+++
@@ -1,8 +0,0 @@
-from utils import shell_cmd
-
-
-def test_download_data():
-    cmd = "python icon_vis/icon_vis/modules/get_data.py"
-    status, _ = shell_cmd(cmd)
-
-    assert status == 0, "Could not download data from ftp-server"
--- testsuite/test_transect.py
+++
@@ -1,6 +0,0 @@
-from utils import shell_cmd
-
-
-def test_transect():
-    status, _ = shell_cmd("python transect/plot_icon_transects.py")
-    assert status == 0, "psy-transect does not run"
