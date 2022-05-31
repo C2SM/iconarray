e6b4a1e0b6598a52967cc7abdf68dcc3c4d9a6cf
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-05-31 14:21:38 +0200

    formatoptions in import in plot folder

---
+++ iconarray/plot/__init__.py
@@ -0,0 +1,6 @@
+from .formatoptions import borders
+from .formatoptions import customtext
+from .formatoptions import lakes
+from .formatoptions import meanmaxwind
+from .formatoptions import rivers
+from .formatoptions import standardtitle
