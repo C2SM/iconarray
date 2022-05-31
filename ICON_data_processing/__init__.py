8169454f1e9c989684a68dae9fcdfd9d476b7c96
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-05-31 10:43:11 +0200

    imports in __init__.py and move test folder

--- ICON_data_processing/__init__.py
+++ ICON_data_processing/__init__.py
@@ -1,5 +1,9 @@
-# from .config import get_several_input, read_config
-# from .get_data import get_example_data
-# from .grid import check_grid_information, combine_grid_information, open_dataset
-# from .interpolate import remap_ICON_to_ICON, remap_ICON_to_regulargrid
-# from .utils import add_coordinates, get_stats, ind_from_latlon, show_data_vars, wilks
+from backend.grid import check_grid_information, combine_grid_information, open_dataset
+
+from core.interpolate import remap_ICON_to_ICON, remap_ICON_to_regulargrid
+from core.utilities import add_coordinates, get_stats, ind_from_latlon, show_data_vars, wilks
+
+from plot.config import get_several_input, read_config
+
+from utils.get_data import get_example_data
+
--- ICON_data_processing/plot/__init__.py
+++ ICON_data_processing/plot/__init__.py
@@ -1,2 +1,1 @@
 from .formatoptions import *
-from .modules import *
