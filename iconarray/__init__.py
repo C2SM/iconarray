79fb7ef1e660d7843d54570bfe04add882679c04
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-05-31 14:05:13 +0200

    fix init.py

--- iconarray/__init__.py
+++ iconarray/__init__.py
@@ -1,9 +1,9 @@
-from backend.grid import check_grid_information, combine_grid_information, open_dataset
+from .backend.grid import check_grid_information, combine_grid_information, open_dataset
 
-from core.interpolate import remap_ICON_to_ICON, remap_ICON_to_regulargrid
-from core.utilities import add_coordinates, get_stats, ind_from_latlon, show_data_vars, wilks
+from .core.interpolate import remap_ICON_to_ICON, remap_ICON_to_regulargrid
+from .core.utilities import add_coordinates, get_stats, ind_from_latlon, show_data_vars, wilks
 
-from plot.config import get_several_input, read_config
+from .plot.config import get_several_input, read_config
 
-from utils.get_data import get_example_data
+from .utils.get_data import get_example_data
 
