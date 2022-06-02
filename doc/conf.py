6a842383777c78383b56f86b209e5787ccf46155
Victoria Cherkas <87643948+victoria-cherkas@users.noreply.github.com>
2022-06-02 14:09:57 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 current_dir = os.path.dirname(__file__)
-target_dir = os.path.abspath(os.path.join(current_dir, "../"))
+target_dir = os.path.abspath(os.path.join(current_dir, "."))
 sys.path.insert(0, target_dir)
 
 print('target_dir:', end=" ")
