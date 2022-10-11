769cb981e7a2de77e330baabc88277c8b0e331c7
Claire Merker <claire.merker@meteoswiss.ch>
2022-10-11 13:32:35 +0200

    Add tests for select_data().

---
+++ iconarray/tests/test_select_data.py
@@ -0,0 +1,131 @@
+"""
+This module contains tests for the function select_data().
+
+Contains tests: test_file, test_ds, , test_getall, test_with_grid
+"""
+
+import iconarray
+
+file_native = "./data/example_data/grib/lfff00000000z_vn"  # unstructured grid
+file_latlon = "./data/example_data/grib/i1effsurf000_001"  # latlon grid
+file_nc = "./data/example_data/nc/icon_19790101T000000Z.nc"  # netcdf unstructured grid
+file_grid = "./data/example_data/grids/domain1_DOM01_r19b07.nc"
+
+
+def test_file():
+    """
+    Test the the select_data() function with existing and non-existing variables.
+
+    The variables are selected from different hypercubes. Tests are performed using
+    file paths as input for select_data().
+    """
+    # grib unstructured grid
+    vars = ["T", "CLAT", "CLON", "BLUE"]
+    ds_dict = iconarray.select_data(file_native, vars)
+    assert sorted(list(ds_dict.keys())) == [
+        "CLAT",
+        "CLON",
+        "T"
+    ], "ds_dict should have the variables ['CLAT', 'CLON', 'T']"
+    assert type(ds_dict["T"]).__name__ == "Dataset", "type name of ds_dict['T'] should be Dataset"
+
+    # grib latlon grid
+    vars = ["T", "CAPE_ML", "CIN_ML", "BLUE"]
+    ds_dict = iconarray.select_data(file_latlon, vars)
+    assert sorted(list(ds_dict.keys())) == [
+        "CAPE_ML",
+        "CIN_ML",
+        "T",
+    ], "ds_dict should have the variables ['CAPE_ML', 'CIN_ML', 'T']"
+    assert type(ds_dict["T"]).__name__ == "Dataset", "type name of ds_dict['T'] should be Dataset"
+
+    # netcdf unstructured grid
+    vars = ["temp", "t_so", "BLUE"]
+    ds_dict = iconarray.select_data(file_nc, vars)
+    assert sorted(list(ds_dict.keys())) == [
+        "t_so",
+        "temp",
+    ], "ds_dict should have the variables ['t_so', 'temp']"
+    assert type(ds_dict["temp"]).__name__ == "Dataset", "type name of ds_dict['temp'] should be Dataset"
+
+
+def test_ds():
+    """
+    Test the the select_data() function with existing and non-existing variables.
+
+    The variables are selected from different hypercubes. Tests are performed using
+    the open_dataset() to first read the data and calling select_data() on the
+    output.
+    """
+    # grib unstructured grid
+    vars = ["T", "CLAT", "CLON", "BLUE"]
+    ds = iconarray.open_dataset(file_native)
+    ds_dict = iconarray.select_data(ds, vars)
+    assert sorted(list(ds_dict.keys())) == [
+        "CLAT",
+        "CLON",
+        "T"
+    ], "ds_dict should have the variables ['CLAT', 'CLON', 'T']"
+    assert type(ds_dict["T"]).__name__ == "Dataset", "type name of ds_dict['T'] should be Dataset"
+
+    # grib latlon grid
+    vars = ["T", "CAPE_ML", "CIN_ML", "BLUE"]
+    ds = iconarray.open_dataset(file_latlon)
+    ds_dict = iconarray.select_data(ds, vars)
+    assert sorted(list(ds_dict.keys())) == [
+        "CAPE_ML",
+        "CIN_ML",
+        "T",
+    ], "ds_dict should have the variables ['CAPE_ML', 'CIN_ML', 'T']"
+    assert type(ds_dict["T"]).__name__ == "Dataset", "type name of ds_dict['T'] should be Dataset"
+
+    # netcdf unstructured grid
+    vars = ["temp", "t_so", "BLUE"]
+    ds = iconarray.open_dataset(file_nc)
+    ds_dict = iconarray.select_data(ds, vars)
+    assert sorted(list(ds_dict.keys())) == [
+        "t_so",
+        "temp",
+    ], "ds_dict should have the variables ['t_so', 'temp']"
+    assert type(ds_dict["temp"]).__name__ == "Dataset", "type name of ds_dict['temp'] should be Dataset"
+
+
+def test_getall():
+    """
+    Test the the select_data() function to select all variables in the file.
+    """
+    # grib unstructured grid
+    vars = []
+    ds_dict = iconarray.select_data(file_native, vars)
+    assert sorted(list(ds_dict.keys())) == [
+        "CLAT",
+        "CLON",
+        "ELAT",
+        "ELON",
+        "T",
+        "U",
+        "V",
+        "VLAT",
+        "VLON",
+    ], "ds_dict should have the variables ['CLAT', 'CLON', 'ELAT', 'ELON', 'T', 'U', 'V', 'VLAT', 'VLON']"
+
+
+def test_with_grid():
+    """
+    Test the the select_data() function with specification of a grid file.
+
+    The test also tests if the specify variable can be a string.
+    """
+    # grib unstructured grid
+    vars = "T"
+    ds_dict = iconarray.select_data(file_native, vars, grid_file=file_grid)
+    assert (
+        sum(
+            [
+                1
+                for coord in ["clon", "clat"]
+                if coord in ds_dict["T"].coords
+            ]
+        )
+        == 2
+    ), "ds_dict should have coordinates 'clon', 'clat'"
