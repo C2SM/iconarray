d23ac5057b0959243ff304775e07bbfc6315f090
Simon Adamov <simon.adamov@mailbox.org>
2022-11-15 17:33:37 +0100

    Capturing all remaining statements with if clauses Implementing Feedback from victoria-cherkas

--- env/setup-conda-env.sh
+++ env/setup-conda-env.sh
@@ -2,17 +2,28 @@
 
 if [[ $(hostname -s) == *'tsa'* ]]; then
     echo 'Setting GRIB_DEFINITION_PATH for cfgrib engine'
-    errormessage=$(module load python)
-    if [ -z "$errormessage" ]; then
-        echo 'EasyBuild loaded the python module successfully.'
+    if module load python; then
+        echo '"module load python" was successful.'
     else
-        echo 'EasyBuild was not able to load the python module.'
+        echo '"module load python" was NOT successful. Please contact your system admin.'
         return $1
     fi
     source /project/g110/spack/user/admin-tsa/spack/share/spack/setup-env.sh
 
-    cosmo_eccodes=$(spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc | head -n1)
-    eccodes=$(spack find --format "{prefix}" eccodes@2.19.0%gcc \~aec | head -n1)
+    if cosmo_eccodes=$(spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc | head -n1); then
+        echo 'Cosmo eccodes-definitions were set successfully.'
+    else
+        echo 'Cosmo eccodes-definitions could not be set properly. Please check your Spack setup.'
+        return $1
+    fi
+
+    if eccodes=$(spack find --format "{prefix}" eccodes@2.19.0%gcc \~aec | head -n1); then
+        echo 'Eccodes were successfully retrieved.'
+    else
+        echo 'Eccodes retrieval failed. Please check your Spack setup.'
+        return $1
+    fi
+
     export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
     export OMPI_MCA_pml="ucx"
     export OMPI_MCA_osc="ucx"
@@ -21,17 +32,28 @@ if [[ $(hostname -s) == *'tsa'* ]]; then
 
 elif [[ $(hostname -s) == *'daint'* ]]; then
     echo 'Setting GRIB_DEFINITION_PATH for cfgrib engine'
-    errormessage=$(module load cray-python)
-    if [ -z "$errormessage"]; then
-        echo 'EasyBuild loaded the python module successfully.'
+    if module load cray-python; then
+        echo '"module load cray-python" was successful.'
     else
-        echo 'EasyBuild was not able to load the python module.'
+        echo '"module load cray-python" was NOT successful. Please contact your system admin.'
         return $1
     fi
     source /project/g110/spack/user/admin-daint/spack/share/spack/setup-env.sh
 
-    cosmo_eccodes=$(spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc | head -n1)
-    eccodes=$(spack find --format "{prefix}" eccodes@2.19.0%gcc \~aec | head -n1)
+    if cosmo_eccodes=$(spack find --format "{prefix}" cosmo-eccodes-definitions@2.19.0.7%gcc | head -n1); then
+        echo 'Cosmo eccodes-definitions were set successfully.'
+    else
+        echo 'Cosmo eccodes-definitions could not be set properly. Please check your Spack setup.'
+        return $1
+    fi
+
+    if eccodes=$(spack find --format "{prefix}" eccodes@2.19.0%gcc \~aec | head -n1); then
+        echo 'Eccodes were successfully retrieved.'
+    else
+        echo 'Eccodes retrieval failed. Please check your Spack setup.'
+        return $1
+    fi
+
     export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
     export OMPI_MCA_pml="ucx"
     export OMPI_MCA_osc="ucx"
@@ -55,12 +77,25 @@ fi
 
 # ---- required for cartopy ------
 
-python_version=$(python --version | tr '[:upper:]' '[:lower:]' | sed 's/ //g' | sed 's/\.[^.]*$//')
-vpython=$(ls --color=never -d $CONDA_PREFIX/lib/$python_version)
+if python_version=$(python --version | tr '[:upper:]' '[:lower:]' | sed 's/ //g' | sed 's/\.[^.]*$//'); then
+    echo $python_version
+else
+    echo "Please check your Python version and make sure that the appropriate Conda env is activated."
+    return $1
+fi
+if vpython=$(ls --color=never -d $CONDA_PREFIX/lib/$python_version); then
+    echo $vpython
+else
+    echo "Please check your Python binary path and make sure that the appropriate Conda env is activated."
+    return $1
+fi
 if cp env/siteconfig.py ${vpython}/site-packages/cartopy; then
-    echo -e 'The setup script completed successfully! \n' \
-        'Make sure to deactivate your environment completely before reactivating it by running "conda deactivate" twice.'
+    echo 'Cartopy configuration completed successfully.'
 else
     echo -e 'Enable cartopy to modify cartopy.config by placing the env/siteconfig.py file into cartopy package source folder. \n' \
         'Please make sure that you are in the parent directory of the iconarray folder while executing this setup script.'
+    return $1
 fi
+
+echo -e 'The setup script completed successfully! \n' \
+    'Make sure to deactivate your environment completely before reactivating it by running "conda deactivate" twice.'
