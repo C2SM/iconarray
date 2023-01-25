e0488b321ac38478a38e660d4e17ad91c007aa40
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2023-01-25 11:08:44 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="iconarray",  # Required
-    version="v0.2.0",  # Required
+    version="v0.2.1",  # Required
     description="A package of modules for processing and plotting ICON data.",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional
