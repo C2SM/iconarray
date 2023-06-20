a544328ce2c0272a1261c77bd04984ac903b7bdf
Claire Merker <claire.merker@meteoswiss.ch>
2023-06-20 18:47:48 +0200

    add test for raise in _open_GRIB

--- iconarray/tests/fast_tests.py
+++ iconarray/tests/fast_tests.py
@@ -1,12 +1,15 @@
 """A collection of fast tests."""
 
 import codecs
-from unittest.mock import Mock, mock_open
+import cfgrib
+from unittest.mock import Mock, mock_open, patch
 
 import pytest
 
-from iconarray.backend.grid import _identify_datatype, _identifyGRIB, _identifyNC
-
+from iconarray.backend.grid import _identify_datatype
+from iconarray.backend.grid import _identifyGRIB
+from iconarray.backend.grid import _identifyNC
+from iconarray.backend.grid import _open_GRIB
 
 @pytest.mark.parametrize(
     "file_nc,file_gb,expected",
@@ -65,3 +68,34 @@ def test_identifyGRIB(open_mock, expected, request):
     _ = request.getfixturevalue(open_mock)
     assert _identifyGRIB("file.grib") == expected
     codecs.open.assert_called_once()
+
+
+@pytest.fixture
+def mocker_open_gribfile(mocker):
+    """Mock cfgrib.open_datasets()."""
+    # noqa: DAR101
+    mocked_gribfile = mock_open(read_data="grib mocked data")
+    mocker.patch("cfgrib.open_datasets", mocked_gribfile)
+    cfgrib.open_datasets.seide_effect = KeyError("paramId")
+
+
+def test_open_GRIB_raise(mocker):
+    """Test if _open_GRIB raises correclty."""
+    # noqa: DAR101
+    mocked_gribfile = mock_open(read_data="grib mocked data")
+    mocker.patch("cfgrib.open_datasets", mocked_gribfile)
+    cfgrib.open_datasets.side_effect = KeyError("paramId")
+
+    mocked_function = Mock()
+    mocked_function.return_value = "var1, var2"
+
+    with pytest.raises(KeyError) as e:
+        with patch('iconarray.backend.grid.show_GRIB_shortnames', new=mocked_function):
+             _open_GRIB("file", "variable", decode_coords={}, decode_times={}, backend_kwargs={})
+    assert e.exconly().startswith('KeyError: "Cannot filter dataset by variable')
+
+    cfgrib.open_datasets.side_effect = KeyError("else")
+    with pytest.raises(KeyError) as e:
+        with patch('iconarray.backend.grid.show_GRIB_shortnames', new=mocked_function):
+             _open_GRIB("file", "variable", decode_coords={}, decode_times={}, backend_kwargs={})
+    assert e.exconly() == "KeyError: 'else'"
